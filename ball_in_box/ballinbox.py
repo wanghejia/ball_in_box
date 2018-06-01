d98093cbb543c4ed8f638fe1395f269652f93ee5
wanghejia <39829372+wanghejia@users.noreply.github.com>
2018-06-01 19:54:56 +0800

    Update ballinbox.py

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -1,32 +1,54 @@
 import math
 import random
-from .validate import validate
+import validate as vl
 
 __all__ = ['ball_in_box']
 
-def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
-    """
-    m is the number circles.
-    n is the list of coordinates of tiny blocks.
-    
-    This returns a list of tuple, composed of x,y of the circle and r of the circle.
-    """
+def get_r(x,y,blockers,circles):
+    r = 1 - x
+    if r>(1 - y):
+        r = 1 - y
+    if r>(x + 1):
+        r = x + 1
+    if r>(y + 1):
+        r = y + 1
+    for blocker in blockers:
+        x1 = blocker[0]
+        y1 = blocker[1]
+        r1 = ((x1-x)**2+(y1-y)**2)**0.5
+        if r>r1:
+            r = r1
+    for circle in circles:
+        x2 = circle[0]
+        y2 = circle[1]
+        r2 = circle[2]
+        r3 = ((x2-x)**2+(y2-y)**2)**0.5 - r2
+        if r>r3:
+            r = r3
+    circles2 = circles + [(x,y,0)]
+    return r
 
-    # The following is an example implementation.
+def judge(x,y,circles):
+    for circle in circles:
+        x1 = circle[0]
+        y1 = circle[1]
+        r = circle[2]
+        rr = ((x1-x)**2+(y1-y)**2)**0.5
+        if rr<r:
+            return False
+    return True
+
+
+def ball_in_box(m, blockers):
     circles = []
-    for circle_index in range(m):
-
+    i = 0
+    while i<m:
         x = random.random()*2 - 1
         y = random.random()*2 - 1
-        r = random.random()*0.1
-
-        circles.append((x, y, r))
-        while not validate(circles, blockers):
+        while not judge(x,y,circles):
             x = random.random()*2 - 1
             y = random.random()*2 - 1
-            r = random.random()*0.1
-            circles[circle_index] = (x, y, r)
-
-        circle_index += 1
-    
+        r = get_r(x,y,blockers,circles)
+        circles.append((x,y,r))
+        i += 1
     return circles
